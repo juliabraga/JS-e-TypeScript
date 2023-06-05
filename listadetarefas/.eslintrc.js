22259f5500267e24ddd4371d147f04818f25f723
juliabraga <juliabraga@gmail.com>
2023-06-05 08:28:48 -0300

    Criado Apagar e Editar

---
+++ listadetarefas/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+  },
+};
